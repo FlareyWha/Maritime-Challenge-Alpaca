ff9db1bfce784cbe403ec991f2b135df75f32f12
gq <guanquntan@gmail.com>
2022-08-22 17:32:40 +0800

    scenemanager

---
+++ Maritime Challenge/Assets/Scripts/SceneManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManager : MonoBehaviourSingleton<SceneManager>
+{
+    public void LoadScene(string name)
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(name);
+    }
+
+    public void LoadScene(SCENE_ID id)
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene((int)id);
+    }
+
+}
+
+public enum SCENE_ID
+{
+    SPLASH = 0,
+    LOGIN = 1,
+    HOME = 2,
+
+    NUM_TOTAL
+}
