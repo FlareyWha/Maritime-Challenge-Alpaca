d91911cab8fbff0e397af66836739bacc2680536
gq <guanquntan@gmail.com>
2022-08-23 15:13:49 +0800

    imported mirror and setup connection

--- Maritime Challenge/Assets/Scripts/LoginManager.cs
+++ Maritime Challenge/Assets/Scripts/LoginManager.cs
@@ -1,11 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Mirror;
 
 public class LoginManager : MonoBehaviour
 {
+    NetworkManager manager;
+
+    void Start()
+    {
+        manager = GetComponent<NetworkManager>();
+    }
+
     public void Login()
     {
+        // Check Verification Database blah blah...
 
+
+
+        // LOGIN
+        // Set Player Details/Data
+
+        // Connec to Server
+        SceneManager.Instance.LoadScene(SCENE_ID.HOME);
+        manager.StartClient();
     }
 }
