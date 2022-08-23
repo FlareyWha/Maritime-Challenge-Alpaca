5f38cd4be2f8159cbd2a02b962e4b68c0bae6ebf
gq <guanquntan@gmail.com>
2022-08-23 15:18:07 +0800

    SPLASHSCREEN

--- Maritime Challenge/Assets/Scripts/SplashScreen.cs
+++ Maritime Challenge/Assets/Scripts/SplashScreen.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class SplashScreen : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    public float wait_time = 1.0f;
+
     void Start()
     {
-        
+        StartCoroutine(SplashcreenWait());
     }
 
-    // Update is called once per frame
-    void Update()
+
+    IEnumerator SplashcreenWait()
     {
-        
+        yield return new WaitForSeconds(wait_time);
+
+        SceneManager.Instance.LoadScene(SCENE_ID.LOGIN);
     }
 }
