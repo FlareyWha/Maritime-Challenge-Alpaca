658e561480164ea9834e17900ebf97bb07d093f1
gq <guanquntan@gmail.com>
2022-08-25 16:43:21 +0800

    aaaaaaaaaaaaaaaaaa

--- Maritime Challenge/Assets/Scripts/Player/PlayerData.cs
+++ Maritime Challenge/Assets/Scripts/Player/PlayerData.cs
@@ -7,5 +7,5 @@ public static class PlayerData
     public static string Name = "Unset";
     public static int UID = 0;
     public static int NumRightshipRollers = 0;
-    public static Player MyPlayer = null;\
+    public static Player MyPlayer = null;
 }
