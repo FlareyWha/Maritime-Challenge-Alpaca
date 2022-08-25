482f8d5f4071fc2409fc51ab951fbd4086ec1cca
FlareyWha <69341849+FlareyWha@users.noreply.github.com>
2022-08-25 17:13:47 +0800

    split the loginManager and networkManager

--- Maritime Challenge/Assets/Scripts/Database/ServerDataManager.cs
+++ Maritime Challenge/Assets/Scripts/Database/ServerDataManager.cs
@@ -6,7 +6,7 @@ public class ServerDataManager : MonoBehaviourSingleton<ServerDataManager>
 {
     //Server url that will change
     static string serverURL = "http://localhost/Maritime_Challenge/";
-    //static string serverURL = "http://ec2-107-20-224-130.compute-1.amazonaws.com/lab02_daevon_200412T/";
+    //static string serverURL = "http://ship.focused.lol/Maritime_Challenge/";
 
     //URL's
     public string URL_getUsername = serverURL + "GetUsername.php";
