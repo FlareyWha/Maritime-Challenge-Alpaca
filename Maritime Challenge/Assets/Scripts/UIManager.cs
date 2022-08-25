b7bcf97973636370de342f3941d8d683725c651e
gq <guanquntan@gmail.com>
2022-08-25 13:27:05 +0800

    chat UI

--- Maritime Challenge/Assets/Scripts/UIManager.cs
+++ Maritime Challenge/Assets/Scripts/UIManager.cs
@@ -10,6 +10,7 @@ public class UIManager : MonoBehaviourSingleton<UIManager>
     public Joystick Joystick;
 
     public Image MenuPanelMask;
+    public Image ChatPanelMask;
 
 
     private const float OPEN_MENU_ANIM_TIME = 0.5f;
@@ -21,46 +22,50 @@ public class UIManager : MonoBehaviourSingleton<UIManager>
     public void ToggleMenu(Button button)
     {
         if (MenuPanelMask.fillAmount == 0.0f)
-            StartCoroutine(OpenMenuAnim(button));
+            StartCoroutine(ToggleSlideAnim(MenuPanelMask, true, button));
         else
-            StartCoroutine(CloseMenuAnim(button));
+            StartCoroutine(ToggleSlideAnim(MenuPanelMask, false, button));
     }
 
-    IEnumerator OpenMenuAnim(Button button)
+   
+
+    public void ToggleChat(Button button)
+    {
+        if (ChatPanelMask.fillAmount == 0.0f)
+            StartCoroutine(ToggleSlideAnim(ChatPanelMask, true, button));
+        else
+            StartCoroutine(ToggleSlideAnim(ChatPanelMask, false, button));
+    }
+
+    IEnumerator ToggleSlideAnim(Image mask, bool open, Button button)
     {
         button.interactable = false;
+        if (open)
+            mask.gameObject.SetActive(true);
 
         float fade_rate = 1.0f / OPEN_MENU_ANIM_TIME;
+        if (!open)
+            fade_rate *= -1;
 
         timer = OPEN_MENU_ANIM_TIME;
         while (timer > 0.0f)
         {
             timer -= Time.deltaTime;
-            MenuPanelMask.fillAmount += fade_rate * Time.deltaTime;
+            mask.fillAmount += fade_rate * Time.deltaTime;
             yield return null;
         }
 
-        MenuPanelMask.fillAmount = 1.0f;
+        if (open)
+            mask.fillAmount = 1.0f;
+        else
+            mask.fillAmount = 0.0f;
+
 
         button.interactable = true;
+        if (!open)
+            mask.gameObject.SetActive(false);
     }
 
-    IEnumerator CloseMenuAnim(Button button)
-    {
-        button.interactable = false;
 
-        float fade_rate = 1.0f / CLOSE_MENU_ANIM_TIME;
 
-        timer = CLOSE_MENU_ANIM_TIME;
-        while (timer > 0.0f)
-        {
-            timer -= Time.deltaTime;
-            MenuPanelMask.fillAmount -= fade_rate * Time.deltaTime;
-            yield return null;
-        }
-
-        MenuPanelMask.fillAmount = 0.0f;
-
-        button.interactable = true;
-    }
 }
