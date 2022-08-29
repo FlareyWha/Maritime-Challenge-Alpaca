18440ec46d101e0b83359455954397ec9a7cb8d3
gq <guanquntan@gmail.com>
2022-08-29 17:03:26 +0800

    textbox expandddddddddddddddddddd

--- Maritime Challenge/Assets/Scripts/UI/ChatBubbleUI.cs
+++ Maritime Challenge/Assets/Scripts/UI/ChatBubbleUI.cs
@@ -11,5 +11,13 @@ public class ChatBubbleUI : MonoBehaviour
     public void Init(string message)
     {
         MessageText.text = message;
+
+
+
+        float text_height = MessageText.preferredHeight;
+        Debug.Log("Text Height: " + text_height);
+        RectTransform rt = GetComponent(typeof(RectTransform)) as RectTransform;
+        rt.sizeDelta = new Vector2(rt.rect.width, text_height);
+
     }
 }
