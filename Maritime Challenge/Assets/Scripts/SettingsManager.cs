7d5ab209dadb7f4999c2b4474662c402cbd35694
gq <guanquntan@gmail.com>
2022-08-30 13:15:23 +0800

    wip

---
+++ Maritime Challenge/Assets/Scripts/SettingsManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingsManager : MonoBehaviour
+{
+    [SerializeField]
+    private Toggle JoystickToggle;
+    
+    void Start()
+    {
+        
+    }
+
+}
