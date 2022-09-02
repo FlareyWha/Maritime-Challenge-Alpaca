1b21680b8c9184e478736f12fa2544a8df5de743
FlareyWha <69341849+FlareyWha@users.noreply.github.com>
2022-09-02 09:38:01 +0800

    implemented the changing of icons when hiding and showing password

--- Maritime Challenge/Assets/Scripts/UI/PasswordShowHide.cs
+++ Maritime Challenge/Assets/Scripts/UI/PasswordShowHide.cs
@@ -7,14 +7,25 @@ public class PasswordShowHide : MonoBehaviour
 {
     [SerializeField]
     private InputField passwordInputField;
+    [SerializeField]
+    private Image passwordShowHideButtonImage;
+
+    [SerializeField]
+    private Sprite passwordHideIcon, passwordShowIcon;
 
     public void TogglePasswordVisibility()
     {
         //Toggles between the 2 depending on which type it currently is
         if (passwordInputField.contentType == InputField.ContentType.Password)
+        {
             passwordInputField.contentType = InputField.ContentType.Standard;
+            passwordShowHideButtonImage.sprite = passwordShowIcon;
+        }
         else
+        {
             passwordInputField.contentType = InputField.ContentType.Password;
+            passwordShowHideButtonImage.sprite = passwordHideIcon;
+        }
 
         passwordInputField.ForceLabelUpdate();
     }
