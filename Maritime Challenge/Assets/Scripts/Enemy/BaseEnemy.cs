dc51c872040dfeb8e4512b74e54ee0fba69fe26f
FlareyWha <69341849+FlareyWha@users.noreply.github.com>
2022-09-09 11:33:46 +0800

    added a package that simulates mobile, and added enemy prefab and updated baseEnemy class

--- Maritime Challenge/Assets/Scripts/Enemy/BaseEnemy.cs
+++ Maritime Challenge/Assets/Scripts/Enemy/BaseEnemy.cs
@@ -13,10 +13,12 @@ public enum ENEMY_STATES
 
 public class BaseEnemy : BaseEntity
 {
+    [SerializeField]
     protected Vector2 spawnPoint;
     protected float distanceToSpawnPoint;
     [SerializeField]
-    protected float maxDistanceToSpawnPoint = 30f;
+    protected int movementAreaCellWidth, movementAreaCellHeight;
+    protected Vector2 movementAreaLowerLimit, movementAreaUpperLimit;
 
     protected Player currentTargetPlayer;
     protected Vector2 directionToPlayer;
@@ -33,10 +35,11 @@ public class BaseEnemy : BaseEntity
     [SerializeField]
     protected float maxChaseTime = 5f;
 
-    private float timer;
-    private float maxTimer;
+    protected float timer;
+    protected float maxTimer;
     protected ENEMY_STATES currEnemyState;
 
+    protected float aStarTimer;
     protected List<Vector3Int> path;
     protected Vector3 destination;
     protected Vector3 currentMovementDirection;
@@ -45,6 +48,16 @@ public class BaseEnemy : BaseEntity
     // Start is called before the first frame update
     protected virtual void Start()
     {
+        transform.position = spawnPoint;
+
+        Grid grid = GameObject.Find("Grid").GetComponent<Grid>();
+
+        Vector3Int spawnPointGrid = grid.WorldToCell(spawnPoint);
+
+        //Set the limits for where the enemy can go
+        movementAreaLowerLimit = grid.CellToWorld(new Vector3Int(spawnPointGrid.x - movementAreaCellWidth / 2, spawnPointGrid.y - movementAreaCellHeight / 2, 0));
+        movementAreaUpperLimit = grid.CellToWorld(new Vector3Int(spawnPointGrid.x + movementAreaCellWidth / 2, spawnPointGrid.y + movementAreaCellHeight / 2, 0));
+
         FindPlayerToTarget();
     }
 
@@ -120,7 +133,7 @@ public class BaseEnemy : BaseEntity
     {
         distanceToSpawnPoint = Vector3.Distance(spawnPoint, transform.position);
 
-        if (timer >= maxTimer || distanceToSpawnPoint >= maxDistanceToSpawnPoint || distanceToPlayer > detectionDistance)
+        if (timer >= maxTimer || CheckOutOfBounds() || distanceToPlayer > detectionDistance)
         {
             currEnemyState = ENEMY_STATES.IDLE;
             ResetTimer(maxIdleTime);
@@ -131,7 +144,7 @@ public class BaseEnemy : BaseEntity
 
     protected virtual void HandleAttack()
     {
-
+        //Choose an attack and play its animation or something, once done go into cooldown or go back to chase i suppose
     }
 
     protected void FindPlayerToTarget()
@@ -151,28 +164,53 @@ public class BaseEnemy : BaseEntity
         maxTimer = newMaxTimer;
     }
 
-    protected void Move()
+    protected void CheckAStar()
     {
-        foreach (Vector3Int position in path)
+        if (aStarTimer < 0)
         {
-            if (firstMove)
+            //Get path
+
+            aStarTimer = 1.25f;
+            firstMove = true;
+
+            foreach (Vector3Int position in path)
             {
-                destination = position;
-                currentMovementDirection = destination - transform.position;
-                firstMove = false;
-            }
-            else
-            {
-                if (position - destination == currentMovementDirection)
+                if (firstMove)
                 {
                     destination = position;
+                    currentMovementDirection = destination - transform.position;
+                    firstMove = false;
                 }
                 else
-                    break;
+                {
+                    if (position - destination == currentMovementDirection)
+                    {
+                        destination = position;
+                    }
+                    else
+                        break;
+                }
             }
         }
 
+        aStarTimer -= Time.deltaTime;
+    }
 
-        transform.Translate(directionToPlayer * movespd * Time.deltaTime);
+    protected void Move()
+    {
+        CheckAStar();
+
+        transform.Translate(currentMovementDirection * movespd * Time.deltaTime);
+    }
+
+
+    protected bool CheckOutOfBounds()
+    {
+        if (transform.position.x < movementAreaLowerLimit.x || transform.position.x > movementAreaUpperLimit.x || transform.position.y < movementAreaLowerLimit.y || transform.position.y > movementAreaUpperLimit.y)
+        {
+            return true;
+        }
+
+        return false;
     }
 }
