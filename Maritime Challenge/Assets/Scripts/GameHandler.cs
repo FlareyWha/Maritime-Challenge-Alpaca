5fce3ed5eb2b5773369c7b445604785a3052129f
gq <guanquntan@gmail.com>
2022-09-21 14:14:48 +0800

    online players list added to server

---
+++ Maritime Challenge/Assets/Scripts/GameHandler.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class GameHandler : NetworkBehaviour
+{
+    #region Singleton
+    public static GameHandler Instance = null;
+    #endregion
+    private SyncList<Player> onlinePlayers = new SyncList<Player>();
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    [Server]
+    public void OnNewPlayerJoined(NetworkConnectionToClient conn)
+    {
+        StartCoroutine(SetPlayerOnline(conn));
+    }
+    IEnumerator SetPlayerOnline(NetworkConnectionToClient conn)
+    {
+        while (conn.identity == null)
+            yield return null;
+        onlinePlayers.Add(conn.identity.gameObject.GetComponent<Player>());
+    }
+    [Server]
+    public void OnPlayerQuit(NetworkConnectionToClient conn)
+    {
+        onlinePlayers.Remove(conn.identity.gameObject.GetComponent<Player>());
+       // StartCoroutine(SetPlayerOffline(conn));
+    }
+    //IEnumerator SetPlayerOffline(NetworkConnectionToClient conn)
+    //{
+    //}
+}
