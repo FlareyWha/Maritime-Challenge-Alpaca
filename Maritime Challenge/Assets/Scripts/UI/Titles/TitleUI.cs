33c818104a2efcf0f51273d935697d8fbe927053
gq <guanquntan@gmail.com>
2022-10-04 14:32:45 +0800

    titles ui logic setup

--- Maritime Challenge/Assets/Scripts/UI/Titles/TitleUI.cs
+++ Maritime Challenge/Assets/Scripts/UI/Titles/TitleUI.cs
@@ -1,18 +1,53 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TitleUI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private Image TitleArt;
+    [SerializeField]
+    private GameObject LockIcon, EquippedOverlay;
+    [SerializeField]
+    private Button button;
+
+    private Title title;
+    public Title LinkedTitle { get { return title;  } }
+
+    private Color32 lockedColor = new Color32(100, 100, 120, 255);
+
+    private Action<TitleUI> onSelectAction;
+
+    private void Awake()
     {
-        
+        button.onClick.AddListener(OnTitleSelected);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Init(Title title, bool unlocked)
     {
-        
+        this.title = title;
+        TitleArt.sprite = title.LinkedTitle.TitleSprite;
+
+        button.interactable = unlocked;
+
+        LockIcon.SetActive(!unlocked);
+        if (unlocked)
+            TitleArt.color = Color.white;
+        else
+            TitleArt.color = lockedColor;
     }
+
+
+    private void OnTitleSelected()
+    {
+        ToggleEquippedOverlay(true);
+        onSelectAction?.Invoke(this);
+    }
+
+    public void ToggleEquippedOverlay(bool show)
+    {
+        EquippedOverlay.SetActive(show);
+    }
+
+  
 }
--- Maritime Challenge/Assets/Scripts/UI/Titles/TitlesManager.cs
+++ Maritime Challenge/Assets/Scripts/UI/Titles/TitlesManager.cs
@@ -10,14 +10,47 @@ public class TitlesManager : MonoBehaviour
     [SerializeField]
     private Transform TitlesRect;
 
+    [SerializeField]
+    private Image ProfileDisplayTitle;
+
+
+    private TitleUI currSelected;
+
     void Awake()
     {
-        
+        UpdateTitlesRect();
     }
 
     private void UpdateTitlesRect()
     {
+        foreach (Transform child in TitlesRect)
+        {
+            Destroy(child.gameObject);
+        }
 
+
+        foreach (KeyValuePair<Title, bool> title in PlayerData.titleDictionary)
+        {
+            TitleUI titleUI = Instantiate(TitleUIPrefab, TitlesRect).GetComponent<TitleUI>();
+            titleUI.Init(title.Key, title.Value);
+
+            // If Is Currently Equipped
+            if (PlayerData.CurrentTitleID == title.Key.titleID)
+            {
+                currSelected = titleUI;
+                currSelected.ToggleEquippedOverlay(true);
+            }
+        }
+    }
+
+    private void SwitchTitle(TitleUI newTitle)
+    {
+        currSelected.ToggleEquippedOverlay(false);
+        currSelected = newTitle;
+
+        // set title to newTitle.LinkedTitle in db
+
+        ProfileDisplayTitle.sprite = newTitle.LinkedTitle.LinkedTitle.TitleSprite;
     }
 
  
