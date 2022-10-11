e52ffbca7215b2a0a9369744f5b13b3ba112de0a
gq <guanquntan@gmail.com>
2022-10-11 17:46:29 +0800

    replace some close buttons

--- Maritime Challenge/Assets/Scripts/AvatarDisplay.cs
+++ Maritime Challenge/Assets/Scripts/AvatarDisplay.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class AvatarDisplay : MonoBehaviour
 {
+    [SerializeField]
+    private Image[] AvatarDisplayParts;
   
     private Player player = null;
 
